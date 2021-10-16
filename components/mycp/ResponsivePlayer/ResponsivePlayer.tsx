241377433e27ca3bcec458e6118b3620e9470884
Hazard4233 <60603371+Hazard4233@users.noreply.github.com>
2021-10-16 15:18:35 +0800

    only import youtube player

--- components/mycp/ResponsivePlayer/ResponsivePlayer.tsx
+++ components/mycp/ResponsivePlayer/ResponsivePlayer.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import ReactPlayer from "react-player";
+import ReactPlayer from "react-player/youtube";
 import cn from 'classnames'
 import s from './ResponsivePlayer.module.css'
 
