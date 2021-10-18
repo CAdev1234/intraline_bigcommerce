a7467bc7283e45c40e50a0911767548555f14fea
Hazard4233 <60603371+Hazard4233@users.noreply.github.com>
2021-10-18 17:21:32 +0800

    hubspotform reder 1

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,14 +10,14 @@ const Input = dynamic(import('@components/mycp/Input'))
 const Checkbox = dynamic(import("@components/mycp/Checkbox"))
 
 // import {useHubspotForm} from '@aaronhayes/react-use-hubspot-form';
-// const HubspotForm = dynamic(import('@components/mycp/HubspotForm'))
+const HubspotForm = dynamic(() => import('@components/mycp/HubspotForm'))
 
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { validateEmail } from "@utils/simpleMethod"
 // const validateEmail = dynamic(() => import('@utils/simpleMethod').then((module) => module.validateEmail))
-const { useHubspotForm } = require('@aaronhayes/react-use-hubspot-form');
+// const { useHubspotForm } = require('@aaronhayes/react-use-hubspot-form');
 
 
 export default function ContactUs() {
@@ -65,7 +65,6 @@ export default function ContactUs() {
     }
 
     const closeHubspotModal = () => {
-        console.log('ddddddddddd')
         setEnableContactHubspotForm(false);
         (document.querySelector('body') as HTMLBodyElement).style.overflow = 'auto'
     }
@@ -225,14 +224,14 @@ export default function ContactUs() {
                 </div>
             </div>
             
-            {/* <div className={`${enableContactHubspotForm ? 'block' : 'hidden'}`}>
+            {enableContactHubspotForm && <div>
                 <HubspotForm 
                     formId="91cfa806-067a-4a3b-ba8a-d5cbe9ccf0f3" 
                     portalId="2718899" 
                     target="#my-hubspot-form"
                     closeHubspotForm={closeHubspotModal}/>
-            </div>
-             */}
+            </div>}
+            
 
             {/* <div className={`fixed top-15 left-0 w-full h-screen bg-black bg-opacity-50 flex flex-col ${enableContactHubspotForm ? 'block' : 'hidden'}`}>
                 <div className="relative pl-5 pr-2 py-7_5 bg-white mx-auto my-auto flex flex-col
